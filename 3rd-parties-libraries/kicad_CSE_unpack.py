2c49a487430293b59ee785449a7ad361932b12d8
Axel Johansson <axeljohansson@duck.com>
2023-12-22 22:19:37 +0100

    multiple updates for AxxSolder hw 3.0

--- 3rd-parties-libraries/kicad_CSE_unpack.py
+++ 3rd-parties-libraries/kicad_CSE_unpack.py
@@ -12,7 +12,7 @@ def remove(path):
         raise ValueError("file {} is not a file or dir.".format(path))
 
 currentpath = os.getcwd()
-zipfolders = os.getcwd() + "/componentsearchengine_ZIP_archive"
+zipfolders = os.getcwd() + "\\componentsearchengine_ZIP_archive"
 
 tempfolderpath = os.path.join(currentpath, "3D-models")
 if not os.path.exists(tempfolderpath):
@@ -32,39 +32,41 @@ os.makedirs(tempfolderpath, exist_ok=True)
 print("temp directory created %s" % tempfolderpath)
 
 for filename in os.listdir(zipfolders):
-    f = os.path.join(zipfolders, filename)
-    # checking if it is a file
-    if f.endswith('.zip'):
+    try:
+        f = os.path.join(zipfolders, filename)
+        # checking if it is a file
+        if f.endswith('.zip'):
 
-        with zipfile.ZipFile(f, 'r') as zip_ref:
-            zip_ref.extractall(tempfolderpath)
+            with zipfile.ZipFile(f, 'r') as zip_ref:
+                zip_ref.extractall(tempfolderpath)
 
-            if(1==1):#try:
-                file = f.split('LIB_')[1].strip(".zip")+"/3D"
+                file = f.split('LIB_')[1].strip(".zip")+"\\3D"
                 print(file)
-                for filename in os.listdir(tempfolderpath + "/" + file):
-                    if not os.path.isfile(currentpath + "/3D-models/" + filename):
-                        os.replace(os.path.join(tempfolderpath + "/" + file, filename), currentpath + "/3D-models/" + filename)
-                        print("moving file %s" % filename)
-                    else:
-                        print("%s did already exist!" % filename)
-                
-                file = f.split('LIB_')[1].strip(".zip")+"/KiCad"
-                for filename in os.listdir(tempfolderpath + "/" + file):
+                for filename in os.listdir(tempfolderpath + "\\" + file):
+                    try:
+                        if not os.path.isfile(currentpath + "\\3D-models\\" + filename):
+                            os.replace(os.path.join(tempfolderpath + "\\" + file, filename), currentpath + "\\3D-models\\" + filename)
+                            print("moving file %s" % filename)
+                        else:
+                            print("%s did already exist!" % filename)
+                    except Exception as e: print(e)
+
+                file = f.split('LIB_')[1].strip(".zip")+"\\KiCad"
+                for filename in os.listdir(tempfolderpath + "\\" + file):
                     if (filename.endswith('.lib') or filename.endswith('.kicad_sym')):
-                        if not os.path.isfile(currentpath + "/schematic-symbols/" + filename):
-                            os.replace(os.path.join(tempfolderpath + "/" + file, filename), currentpath + "/schematic-symbols/" + filename)
+                        if not os.path.isfile(currentpath + "\\schematic-symbols\\" + filename):
+                            os.replace(os.path.join(tempfolderpath + "\\" + file, filename), currentpath + "\\schematic-symbols\\" + filename)
                             print("moving file %s" % filename)
                         else:
                             print("%s did already exist!" % filename)
                         
-                for filename in os.listdir(tempfolderpath + "/" + file):
+                for filename in os.listdir(tempfolderpath + "\\" + file):
                     if filename.endswith('.kicad_mod') or filename.endswith('.mod'):
-                        if not os.path.isfile(currentpath + "/footprints.pretty/" + filename):
-                            os.replace(os.path.join(tempfolderpath + "/" + file, filename), currentpath + "/footprints.pretty/" + filename)
+                        if not os.path.isfile(currentpath + "\\footprints.pretty\\" + filename):
+                            os.replace(os.path.join(tempfolderpath + "\\" + file, filename), currentpath + "\\footprints.pretty\\" + filename)
                             print("moving file %s" % filename)
                         else:
                             print("%s did already exist!" % filename)
-            #except Exception as e: print(e)
+    except Exception as e: print(e)
 remove(tempfolderpath)
 print("temp directory removed %s" % tempfolderpath)
